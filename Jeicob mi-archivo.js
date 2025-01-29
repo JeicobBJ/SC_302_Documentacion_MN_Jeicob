3cbc265ccb0abd6de9559a50a66453545b32cb0a
JeicobBJ <bonillajeicob2005@gmail.com>
2025-01-28 18:56:05 -0600

    Update Jeicob mi-archivo.js

--- Jeicob mi-archivo.js
+++ Jeicob mi-archivo.js
@@ -2,3 +2,25 @@ alert("hola mundo_editado");
 Console.log(“cambios en el archivo”);
 Commit changes 
   "Agregando el primer archivo JavaScript"
+
+
+<!doctype html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>jQuery  Datepicker </title>
+		  <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+		  <link rel="stylesheet" href="style.css">
+		  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		  <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+		  <script src="jquery.ui.datepicker-es.js"></script>
+		<script>
+		   $(function() {
+		    $( "#datepicker" ).datepicker( $.datepicker.regional[ "Es" ]);
+		   });
+		</script>
+	</head>
+	 <body>
+	    <p>Date: <input type="text" id="datepicker">
+	 </body>
+	</html>
